ab62b6e0e9cb1abefeb491632e47c44eeb4e1604
iamashiqur <getintouch.ashiq@gmail.com>
2024-10-02 16:48:46 +0600

    added tailwindcss classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,21 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sora: ['var(--font-sora)', 'sans-serif'],
+        inter: ['var(--font-inter)', 'sans-serif']
+      },
+      colors: {
+        'clr-14': '#919EAB14'
+      },
+      backgroundImage: {
+        'icon-search': "url('/assets/ic_search.svg')"
+      },
+      boxShadow: {
+        one: '0px 0px 1.742px 0px rgba(145, 158, 171, 0.20), 0px 10.449px 20.899px -3.483px rgba(145, 158, 171, 0.12)'
+      }
+    }
   },
   plugins: []
 }
