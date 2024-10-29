16936e7991f7225ce6343a9bbf0687544a79fb1c
Ashiqur Rahman <getintouch.ashiq@gmail.com>
2024-10-29 09:56:36 +0600

    Fullstack (#1)
    * added dashboad layout
    
    * update vscode settings
    
    * re-arrange folder structure
    
    * modify folder structure
    
    * update vscode settings
    
    * added vscode settings
    
    * modify the issus
    
    * added tailwindcss classes
    
    * update readme file
    
    * update readme file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       fontFamily: {
-        alexandria: '"Alexandria", sans-serif',
+        sora: ['var(--font-sora)', 'sans-serif'],
+        inter: ['var(--font-inter)', 'sans-serif']
+      },
+      colors: {
+        'clr-14': '#919EAB14'
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'icon-search': "url('/assets/ic_search.svg')"
       },
-    },
+      boxShadow: {
+        one: '0px 0px 1.742px 0px rgba(145, 158, 171, 0.20), 0px 10.449px 20.899px -3.483px rgba(145, 158, 171, 0.12)'
+      }
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
